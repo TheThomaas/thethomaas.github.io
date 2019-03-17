10a523042b3b08278623f9ae083533a3abbf3bdf
TheThomaas <48221879+TheThomaas@users.noreply.github.com>
2019-03-17 14:44:05 +0100

    Delete style.js

--- js/style.js
+++
@@ -1,7 +0,0 @@
-var Day = "-webkit-linear-gradient(bottom, #bbbbbb 0%, #ffffff 100%)"; // F0F0F0 000000 bbbbbb
-
-$(document).ready(
-    function Start() {
-        document.getElementsByTagName("body")[0].style.background = Day;
-    }
-);
